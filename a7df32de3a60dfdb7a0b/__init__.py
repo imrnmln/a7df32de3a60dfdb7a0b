6ea1ad4a4890b8adc457cba67d3c5d9547f5ff8e
Zain <115614618+zainantum@users.noreply.github.com>
2024-05-14 09:29:10 +0700

    Update __init__.py

--- a7df32de3a60dfdb7a0b/__init__.py
+++ a7df32de3a60dfdb7a0b/__init__.py
@@ -1977,7 +1977,7 @@ def keep_scroling(
             logging.info("[XPath] Tweet visible currently = %s", len(page_cards))
             if tweet:
                 # check if the tweet is unique
-                tweet_id = .join(str(item) for item in tweet[:-2])
+                tweet_id = "".join(str(item) for item in tweet[:-2])
                 last_date = str(tweet[2])
                 if tweet_id not in tweet_ids:
                     if is_within_timeframe_seconds(last_date, MAX_EXPIRATION_SECONDS):
