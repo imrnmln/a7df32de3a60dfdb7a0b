08409df1dae054db7aefb1d36c0f9632af37afde
Zain <115614618+zainantum@users.noreply.github.com>
2024-05-21 13:44:41 +0700

    Update __init__.py

--- a7df32de3a60dfdb7a0b/__init__.py
+++ a7df32de3a60dfdb7a0b/__init__.py
@@ -2018,6 +2018,15 @@ def keep_scroling(
             driver.execute_script(f"window.scrollTo(0, {random_scroll_position});")
             # driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
             curr_position = driver.execute_script("return window.pageYOffset;")
+            try:
+                WebDriverWait(driver, 5).until(
+                    EC.presence_of_all_elements_located((By.XPATH, "//article[@data-testid='tweet']//div[@data-testid='tweetText']"))
+                )
+                driver.execute_script("""let mediaCards = document.querySelectorAll('[data-testid="card.layoutLarge.media"], [data-testid="tweetPhoto"]');mediaCards.forEach(card => {let prevDiv1 = card.parentElement;if (prevDiv1) prevDiv1.parentElement.style.display = 'none';});""")
+                driver.execute_script("""let repEWl = document.querySelectorAll('[data-testid="reply"]');repEWl.forEach(el => {let prevDiv1 = el.parentElement;if (prevDiv1) prevDiv1.parentElement.style.marginTop = '0';;});""")
+            except:
+                logging.info("Cant execute script....")
+                
             if last_position == curr_position:
                 scroll_attempt += 1
                 # end of scroll region
@@ -2170,6 +2179,15 @@ async def scrape_(
         )
         refresh += 1
         # logging.info("Start execute_script....")
+        try:
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_all_elements_located((By.XPATH, "//article[@data-testid='tweet']//div[@data-testid='tweetText']"))
+            )
+            driver.execute_script("""let mediaCards = document.querySelectorAll('[data-testid="card.layoutLarge.media"], [data-testid="tweetPhoto"]');mediaCards.forEach(card => {let prevDiv1 = card.parentElement;if (prevDiv1) prevDiv1.parentElement.style.display = 'none';});""")
+            driver.execute_script("""let repEWl = document.querySelectorAll('[data-testid="reply"]');repEWl.forEach(el => {let prevDiv1 = el.parentElement;if (prevDiv1) prevDiv1.parentElement.style.marginTop = '0';;});""")
+        except:
+            logging.info("Cant execute script....")
+            
         last_position = driver.execute_script("return window.pageYOffset;")
         scrolling = True
         # logging.info("looking for tweets between " + str(since) + " and " + str(until_local) + " ...")
